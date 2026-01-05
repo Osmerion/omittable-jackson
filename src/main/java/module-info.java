d31b7a2ee18a8f73975f859ce7cbc715cb2fe547
Leon Linhart <themrmilchmann@gmail.com>
2026-01-05 12:53:53 +0100

    fix: correctly require jackson-databind module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,9 @@ import org.jspecify.annotations.NullMarked;
 @NullMarked
 module com.osmerion.omittable.jackson {
 
+    requires transitive com.fasterxml.jackson.databind;
     requires transitive com.osmerion.omittable;
     requires transitive org.jspecify;
-
-    requires static com.fasterxml.jackson.databind;
 
     exports com.osmerion.omittable.jackson;
 
