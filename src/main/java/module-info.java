0f4882da002e08ecb312170a39b05777c452f314
Leon Linhart <themrmilchmann@gmail.com>
2026-01-06 15:05:41 +0100

    chore: document module descriptor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,7 @@
+import com.osmerion.omittable.jackson.OmittableModule;
 import org.jspecify.annotations.NullMarked;
 
+/** Defines the {@link OmittableModule} that provides Jackson integration for omittable types. */
 @NullMarked
 module com.osmerion.omittable.jackson {
 
